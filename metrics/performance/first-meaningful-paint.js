7c5129f8b007e0a545b1e027d8cc0d7390644467
Paul Irish <paul.irish@gmail.com>
2016-04-21 22:01:53 -0700

    Port first meaningful paint metric from ksakamoto's ruby impl

--- metrics/performance/first-meaningful-paint.js
+++ metrics/performance/first-meaningful-paint.js
@@ -16,12 +16,15 @@
  */
 'use strict';
 
-const DevtoolsTimelineModel = require('../../helpers/traces/devtools-timeline-model');
+// const DevtoolsTimelineModel = require('../../helpers/traces/devtools-timeline-model');
 
 const FAILURE_MESSAGE = 'Navigation and first paint timings not found.';
 
-class FirstMeaningfulPaint {
+// Time to First Meaningful Paint: a layout-based approach
+// https://docs.google.com/document/d/1BR94tJdZLsin5poeet0XoTW60M0SjvOJQttKT-JK8HI/edit
 
+// we need trace events like https://codereview.chromium.org/1773633003 to land first.
+class FMP {
   /**
    * @param {!Array<!Object>} traceData
    */
@@ -31,41 +34,94 @@ class FirstMeaningfulPaint {
         return reject(new Error(FAILURE_MESSAGE));
       }
 
-      const model = new DevtoolsTimelineModel(traceData);
-      const events = model.timelineModel().mainThreadEvents();
       let mainFrameID;
       let navigationStart;
       let firstContentfulPaint;
+      let layouts = new Map();
+      let paints = [];
 
-      // Find the start of navigation and our meaningful paint
-      events
-        .filter(e => e.categoriesString.includes('blink.user_timing'))
-        .forEach(event => {
-          // navigationStart == the network begins fetching the page URL
-          // CommitLoad == the first bytes of HTML are returned and Chrome considers
-          //   the navigation a success. A 'isMainFrame' boolean is attached to those events
-          //   However, that flag may be incorrect now, so we're ignoring it.
-          if (event.name === 'navigationStart' && !navigationStart) {
-            mainFrameID = event.args.frame;
-            navigationStart = event;
+      // const model = new DevtoolsTimelineModel(traceData);
+      // const events = model.timelineModel().mainThreadEvents();
+      const events = traceData;
+
+      // Parse the trace for our key events
+      events.filter(e => {
+        return e.cat.includes('blink.user_timing') ||
+          e.name === 'FrameView::performLayout' ||
+          e.name === 'Paint';
+      })
+      .forEach(event => {
+        // navigationStart == the network begins fetching the page URL
+        // CommitLoad == the first bytes of HTML are returned and Chrome considers
+        //   the navigation a success. A 'isMainFrame' boolean is attached to those events
+        //   However, that flag may be incorrect now, so we're ignoring it.
+        if (event.name === 'navigationStart' && !navigationStart) {
+          mainFrameID = event.args.frame;
+          navigationStart = event;
+        }
+        // firstContentfulPaint == the first time that text or image content was
+        // painted. See src/third_party/WebKit/Source/core/paint/PaintTiming.h
+        if (event.name === 'firstContentfulPaint' && event.args.frame === mainFrameID) {
+          firstContentfulPaint = event;
+        }
+        // FIXME: frame argument currently unimplemented. needs upstream fix.
+        if (event.name === 'FrameView::performLayout' && event.args.frame === mainFrameID) {
+          layouts.set(event, event.args.counters);
+        }
+
+        if (event.name === 'Paint' && event.args.data.frame === mainFrameID) {
+          paints.push(event);
+        }
+      });
+
+      function firstMeaningfulPaint(heuristics) {
+        let layoutTime = 0;
+        let maxSoFar = 0;
+        let pending = 0;
+        let paintTime;
+        let significance;
+        layouts.forEach((counters, layoutEvent) => {
+          if (!counters['host'] || counters['visibleHeight'] === 0) {
+            return;
           }
-          // firstContentfulPaint == the first time that text or image content was
-          // painted. See src/third_party/WebKit/Source/core/paint/PaintTiming.h
-          if (event.name === 'firstContentfulPaint' && event.args.frame === mainFrameID) {
-            firstContentfulPaint = event;
+
+          layouts = counters['LayoutObjectsThatHadNeverHadLayout'] || 0;
+
+          significance = ('pageHeight' in heuristics) ? layouts / heightRatio(counters) : layouts;
+
+          if ('webFont' in heuristics && counters['hasBlankText']) {
+            pending += significance;
+          } else {
+            significance += pending;
+            pending = 0;
+            if (significance > maxSoFar) {
+              maxSoFar = significance;
+              layoutTime = layoutEvent.ts;
+            }
           }
         });
-
-      // report the raw numbers
-      if (firstContentfulPaint && navigationStart) {
-        return resolve({
-          navigationStart: /** @type {number} */ (navigationStart.startTime),
-          firstMeaningfulPaint: /** @type {number} */ (firstContentfulPaint.startTime)
-        });
+        paintTime = paints.find(e => e.ts > layoutTime).ts;
+        return paintTime - navigationStart.ts;
       }
-      return reject(new Error(FAILURE_MESSAGE));
+
+      function heightRatio(counters) {
+        const ratioBefore = Math.max(1, counters['contentsHeightBefore'].to_f / counters['visibleHeight']);
+        const ratioAfter = Math.max(1, counters['contentsHeightAfter'].to_f / counters['visibleHeight']);
+        return (ratioBefore + ratioAfter) / 2;
+      }
+
+      var results = {
+        fcp:            firstContentfulPaint.ts - navigationStart.ts,
+        fmpBasic:       firstMeaningfulPaint(),
+        fmpPageHeight:  firstMeaningfulPaint({pageHeight: true}),
+        fmpWebFont:     firstMeaningfulPaint({webFont: true}),
+        fmpFull:        firstMeaningfulPaint({pageHeight: true, webFont: true})
+      }
+      console.log('EFF EMM PEE', results);
+
+      return resolve(results);
     });
   }
 }
 
-module.exports = FirstMeaningfulPaint;
+module.exports = FMP;
