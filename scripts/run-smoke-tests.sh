d288576368f37cb04a8689aa5da0ee4f0d153a97
Paul Irish <paul.irish@gmail.com>
2016-04-25 14:00:42 -0700

    add browserifying the smoke test.

--- scripts/run-smoke-tests.sh
+++ scripts/run-smoke-tests.sh
@@ -1,20 +1,37 @@
 #!/usr/bin/env bash
 
+project_root=$PWD
+
+# setup our testing server
 cd test/fixtures && python -m SimpleHTTPServer 9999 &
 
-./cli/index.js http://localhost:9999/online-only.html > results
+# browserify lighthouse core for the extension
+cd extension && gulp browserify
+if [ $? -eq 1 ]; then
+  echo "Fail! Browserify failed"
+  exit 1
+fi
+cd "$project_root"
 
-if ! grep -q "URL responds with a 200 when offline: false" results; then
+# test a boring page with no service worker
+node cli http://localhost:9999/online-only.html > smoke-results
+
+if ! grep -q "URL responds with a 200 when offline: false" smoke-results; then
   echo "Fail! online only site worked while offline"
   cat results
   exit 1
 fi
 
-sleep 5s
+# if run with --fast then we skip the second test
+if [[ $@ == *'fast'* ]]; then
+	exit 0
+fi
 
-./cli/index.js https://www.moji-brush.com > results
+# test a real offline-enabled app
+sleep 1s
+node cli https://www.moji-brush.com > smoke-results
 
-if ! grep -q "URL responds with a 200 when offline: true" results; then
+if ! grep -q "URL responds with a 200 when offline: true" smoke-results; then
   echo "Fail! offline ready site did not work while offline"
   cat results
   exit 1
