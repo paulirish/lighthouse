1bd57a036bf80d07b04a7f4fc546c1ee64fe7e32
Paul Irish <paul.irish@gmail.com>
2016-04-29 12:14:52 -0700

    lint.

--- src/lib/web-inspector.js
+++ src/lib/web-inspector.js
@@ -176,7 +176,7 @@ WebInspector.DeferredTempFile.prototype = {
 // add support for groupBy('EventName')
 WebInspector.TimelineAggregator.GroupBy.EventName = 'EventName';
 const oldNodeToGroupIdFunction = WebInspector.TimelineAggregator.prototype._nodeToGroupIdFunction;
-WebInspector.TimelineAggregator.prototype._nodeToGroupIdFunction = function (groupBy) {
+WebInspector.TimelineAggregator.prototype._nodeToGroupIdFunction = function(groupBy) {
   if (groupBy === WebInspector.TimelineAggregator.GroupBy.EventName) {
     return node => node.event.name;
   }
