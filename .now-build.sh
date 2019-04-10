5155d8a6c8392eec419d5dfd40e584ccee793d2c
Paul Irish <paulirish@google.com>
2019-04-09 17:01:01 -0700

    use shell entry

---
+++ .now-build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+node -e "
+   console.log('🕒 Generating report for sample_v2.json...');
+   const ReportGenerator = require('./lighthouse-core/report/report-generator');
+   const lhr = require('./lighthouse-core/test/results/sample_v2.json');
+
+   const html = ReportGenerator.generateReportHtml(lhr);
+   const filename = './dist/index.html';
+   fs.writeFileSync(filename, html, {encoding: 'utf-8'});
+   console.log('✅', filename, 'written.')
+"
